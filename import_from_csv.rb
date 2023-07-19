70f332aa0295cdc5823fd668d408da4985a04d81
math <math@Ubuntu.myguest.virtualbox.org>
2023-07-19 18:10:32 -0300

    WIP: Salvando os dados do CSV na database

--- import_from_csv.rb
+++ import_from_csv.rb
@@ -1,11 +1,10 @@
 require 'pg'
 require 'csv'
 
-
 rows = CSV.read("./data.csv", col_sep: ';')
 columns = rows.shift
 
-rows.map do |row|
+rows.map! do |row|
   row.each_with_object({}).with_index do |(cell, acc), idx|
     column = columns[idx]
     acc[column] = cell
@@ -14,55 +13,59 @@ end
 
 $postgresdb = PG.connect(host: 'postgresdb', user: 'admin', password: 'admin')
 
-$postgresdb.exec("CREATE TABLE patients (id SERIAL PRIMARY KEY, 
-                                        cpf VARCHAR, 
-                                        name VARCHAR, 
-                                        email VARCHAR, 
-                                        birth_data VARCHAR, 
-                                        address VARCHAR, 
-                                        city VARCHAR, 
-                                        state VARCHAR)")
+$postgresdb.exec("CREATE TABLE IF NOT EXISTS patients (id SERIAL PRIMARY KEY, 
+                                             cpf VARCHAR, 
+                                             name VARCHAR, 
+                                             email VARCHAR, 
+                                             birth_data VARCHAR, 
+                                             address VARCHAR, 
+                                             city VARCHAR, 
+                                             state VARCHAR)")
 
-$postgresdb.exec("CREATE TABLE exams (crm_doctor VARCHAR, 
-                                     state_crm_doctor VARCHAR(2), 
-                                     name_doctor VARCHAR, 
-                                     email_doctor VARCHAR, 
-                                     token VARCHAR, 
-                                     data_exam VARCHAR, 
-                                     type_exam VARCHAR, 
-                                     limit_type_exam VARCHAR, 
-                                     result_type_exam VARCHAR, 
-                                     patient_id SERIAL REFERENCES patients(id))")
-
-patients = rows.map{ |row| row.first(6) }
-exams = rows.map{|row| row.last(9)}
+$postgresdb.exec("CREATE TABLE IF NOT EXISTS exams (patient_id SERIAL REFERENCES patients(id),
+                                             crm_doctor VARCHAR, 
+                                             state_crm_doctor VARCHAR(2), 
+                                             name_doctor VARCHAR, 
+                                             email_doctor VARCHAR, 
+                                             token VARCHAR, 
+                                             data_exam VARCHAR, 
+                                             type_exam VARCHAR, 
+                                             limit_type_exam VARCHAR, 
+                                             result_type_exam VARCHAR)")
 
 def patient_insert(patient)
   $postgresdb.exec('INSERT INTO patients(cpf, name, email, birth_data, address, city, state) 
-  VALUES ($1, $2, $3, $4, $5, $6, $7)', [patient[0], patient[1], patient[2], patient[3], patient[4], patient[5], patient[6]])
+  VALUES ($1, $2, $3, $4, $5, $6, $7)', 
+  [patient['cpf'], patient['nome paciente'], patient['email paciente'], patient['data nascimento paciente'], 
+   patient['endereço/rua paciente'], patient['cidade paciente'], patient['estado patiente']])
 end
 
 def exam_insert(exam, patient)
   $postgresdb.exec('INSERT INTO exams(crm_doctor, state_crm_doctor, name_doctor, email_doctor, token, data_exam, type_exam, limit_type_exam, result_type_exam, patient_id) 
-  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [exam[7], exam[8] , exam[9], exam[10], exam[11], exam[12], exam[13], exam[14], exam[15], patient[0]['id']])
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', 
+  [exam['crm médico'], exam['crm médico estado'] , exam['nome médico'], exam['email médico'], exam['token resultado exame'], exam['data exame'], 
+   exam['tipo exame'], exam['limites tipo exame'], exam['resultado tipo exame'], patient[0]['id']])
 end
 
 def patient_find(cpf)
   $postgresdb.exec("SELECT * FROM patients WHERE cpf='#{cpf}'").to_a
 end
 
+def doctor_find(cpf)
+  $postgresdb.exec("SELECT * FROM patients WHERE cpf='#{cpf}'").to_a
+end
+
+
 rows.each do |row|
-  patient = patient_find(row[0])
-  
+  patient = patient_find(row['cpf'])
+
   if patient.empty?   
     patient_insert(row)
-    patient = patient_find(row[0])
+    patient = patient_find(row['cpf'])
   end
   
   exam_insert(row, patient)
 end
-
-
 
 # PACIENTE
 # "cpf":"048.973.170-88",
--- server.rb
+++ server.rb
@@ -1,18 +1,34 @@
 require 'sinatra'
 require 'rack/handler/puma'
+require 'pg'
 require 'csv'
 
 get '/tests' do
-  rows = CSV.read("./data.csv", col_sep: ';')
+  $postgresdb = PG.connect(host: 'postgresdb', user: 'admin', password: 'admin')
 
-  columns = rows.shift
+  result =  $postgresdb.exec('SELECT patients.*, exams.crm_doctor, state_crm_doctor, name_doctor, 
+                              email_doctor, token, data_exam, type_exam, limit_type_exam, 
+                              result_type_exam FROM patients INNER JOIN exams ON exams.patient_id = patients.id')
+  
+  result.to_a.to_json
+  
+  # rows = CSV.read("./data.csv", col_sep: ';')
 
-  rows.map do |row|
-    row.each_with_object({}).with_index do |(cell, acc), idx|
-      column = columns[idx]
-      acc[column] = cell
-    end
-  end.to_json
+  # columns = rows.shift
+
+  # rows.map do |row|
+  #   row.each_with_object({}).with_index do |(cell, acc), idx|
+  #     column = columns[idx]
+  #     acc[column] = cell
+  #   end
+  # end.to_json
+end
+
+get '/tests_json' do
+  
+end
+
+get 'instructions' do
 end
 
 get '/hello' do
